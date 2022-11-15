5f2b7f2728924c4248f6dff498585246bb126805
Millena Freitas <59456180+freitasmillena@users.noreply.github.com>
2022-11-15 19:40:07 +0000

    sp comecei udp handler

--- src/SP.java
+++ src/SP.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -117,6 +122,93 @@ public class SP extends Servidor{
                 this.segurancaBD.equals(sp.getSegurancaBD()) &&
                 this.servidoresSecundarios.equals(sp.getServidoresSecundarios()) &&
                 this.BD.equals(sp.getBD()));
+    }
+
+/*
+
+Se calhar é melhor repensar na estrutura do map que guarda as cenas
+Talvez um Map<String, List<Registo>> ?
+ou Map<String, Map<String,Registo>>?
+
+
+    public String fetchMail(){
+
+    }
+
+    public String fetchNS(){
+
+    }
+
+    public String fetchA(){
+
+    }
+    public String handleQuery(PDU query){
+        //se o dominio pedido na query equivale ao dominio tratado pelo SP
+        if(query.getName().equals(super.getDominio())){
+            //verifica tipo e busca response values
+            //busca autoridades
+            //busca A
+
+            //devolve resposta
+        }
+    }
+
+
+
+*/
+
+
+    public void run(){
+        System.out.println("Servidor inicializou");
+        while (true) {
+            byte[] buffer = new byte[256];
+            DatagramSocket serverS = null;
+
+            try {
+                //Receber
+                serverS = new DatagramSocket(5353);
+                DatagramPacket receiver = new DatagramPacket(buffer, buffer.length);
+                serverS.receive(receiver);
+                serverS.close();
+
+                System.out.println("Servidor recebeu");
+
+                int portClient = receiver.getPort();
+                InetAddress ipCliente = receiver.getAddress();
+
+                PDU query = new PDU(receiver.getData());
+
+                //Logs query recebida
+                //Log terminal
+                System.out.println(query);
+                //Log ficheiro
+                Log l1 = new Log("QR", ipCliente.toString(), query.ToString());
+                l1.logToFile(super.getFicheiroLog());
+
+
+                //Procurar resposta
+
+
+
+                //Enviar
+                //String pdu = new PDU
+                DatagramPacket sender = new DatagramPacket(pdu.getBytes(), pdu.getBytes().length, ipCliente, portClient);
+                DatagramSocket s = new DatagramSocket();
+                s.send(sender);
+                s.close();
+
+                //Logs resposta
+                //System.out.println(pdu);
+                //Log l2 = new Log("RP", ipCliente.toString(), pdu);
+                //l2.logToFile(super.getFicheiroLog());
+
+            } catch (SocketException ex) {
+                System.out.println("Socket error: " + ex.getMessage());
+            } catch (IOException ex) {
+                System.out.println("I/O error: " + ex.getMessage());
+            }
+
+        }
     }
 
 }
